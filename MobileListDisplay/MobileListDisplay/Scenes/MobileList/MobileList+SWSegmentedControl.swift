1ce3547ac00c5be83bbf61b05f368923cc841c0a
Sujeet Sinha <sujeet.sinha@allianz.com>
2018-09-29 11:39:15 +0700

    Added initial UI. Customized Segment controller - Change default Tab title

---
+++ MobileListDisplay/MobileListDisplay/Scenes/MobileList/MobileList+SWSegmentedControl.swift
@@ -0,0 +1,37 @@
+//
+//  MobileList+SWSegmentedControl.swift
+//  MobileListDisplay
+//
+//  Created by Sujeet Sinha on 29/9/18.
+//  Copyright © 2018 Sujeet Sinha. All rights reserved.
+//
+
+import SWSegmentedControl
+
+extension MobileListViewController: SWSegmentedControlDelegate {
+  // MARK: - SWSegmentedControlDelegate
+  func segmentedControl(_ control: SWSegmentedControl, willSelectItemAtIndex index: Int) {
+    print("will select \(index)")
+  }
+  
+  func segmentedControl(_ control: SWSegmentedControl, didSelectItemAtIndex index: Int) {
+    print("did select \(index)")
+  }
+  
+  func segmentedControl(_ control: SWSegmentedControl, willDeselectItemAtIndex index: Int) {
+    print("will deselect \(index)")
+  }
+  
+  func segmentedControl(_ control: SWSegmentedControl, didDeselectItemAtIndex index: Int) {
+    print("did deselect \(index)")
+  }
+  
+  func segmentedControl(_ control: SWSegmentedControl, canSelectItemAtIndex index: Int) -> Bool {
+    if index == 1 {
+      return false
+    }
+    
+    return true
+  }
+}
+
